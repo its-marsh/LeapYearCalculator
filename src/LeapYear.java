46bfd9faa97475597e1e7266c15893043e99658b
marsh <m.cowles21191@gmail.com>
2022-10-12 03:43:28 -0400

    fixed leap year calculation

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,6 +1,13 @@
-public class LeapYearCalculator {
+public class LeapYear {
     public static boolean isLeapYear (int year) {
-        boolean isLeapYear = (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0);
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            isLeapYear = (year % 100 != 0);
+            if (year % 100 == 0 && year % 400 == 0) {
+                isLeapYear = true;
+            }
+        }
 
         if (year < 1 || year > 9999) {
             isLeapYear = false;
